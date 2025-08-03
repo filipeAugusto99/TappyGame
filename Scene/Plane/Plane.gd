eceac03f35512b36c32cff5121efd49f839bde40
Filipe Augusto <filipecordeiro213@gmail.com>
2025-08-03 15:29:17 -0300

    Criando a funcao die() e fly() para o aviao

--- Scene/Plane/Plane.gd
+++ Scene/Plane/Plane.gd
@@ -1,15 +1,29 @@
 extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const JUMP: float = -350.0
 
 var _gravity: float = ProjectSettings.get('physics/2d/default_gravity')
 
 func _physics_process(delta: float) -> void:
-	velocity.y += delta * _gravity
+	fly(delta)
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
+	
+	
+func die() -> void:
+	
+	set_physics_process(false)
+	animated_sprite_2d.stop()
+
+func fly(delta: float) -> void:
+	velocity.y += delta * _gravity
 	
 	if Input.is_action_just_pressed('jump') == true:
 		velocity.y = JUMP
 		animation_player.play('jump')
+		
