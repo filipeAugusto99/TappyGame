8a1e8ebe09c8bd316bc29070a64938fdf35d952d
Filipe Augusto <filipecordeiro213@gmail.com>
2025-08-03 15:12:28 -0300

    Animacao do Aviao

--- Scene/Plane/Plane.gd
+++ Scene/Plane/Plane.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 const JUMP: float = -350.0
 
 var _gravity: float = ProjectSettings.get('physics/2d/default_gravity')
@@ -8,5 +10,6 @@ func _physics_process(delta: float) -> void:
 	velocity.y += delta * _gravity
 	move_and_slide()
 	
-	if Input.is_action_just_pressed('jump') == true and is_on_floor() == false:
+	if Input.is_action_just_pressed('jump') == true:
 		velocity.y = JUMP
+		animation_player.play('jump')
