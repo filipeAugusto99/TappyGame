fce704179174d056d6ee172cf8d1da62b4ce1a56
Filipe Augusto <filipecordeiro213@gmail.com>
2025-08-09 14:08:48 -0300

    Spawning Pipes

--- Scene/Game/Game.gd
+++ Scene/Game/Game.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 const PIPES = preload("res://Scene/Pipes/Pipes.tscn")
 
+@onready var pipes_holder: Node = $PipesHolder
+@onready var upper_point: Marker2D = $UpperPoint
+@onready var lower_point: Marker2D = $LowerPoint
+
+
 func _ready() -> void:
 	spawn_pipes()
   
@@ -11,14 +16,16 @@ func _process(delta: float) -> void:
 func spawn_pipes() -> void:
 	var new_pipe: Pipes = PIPES.instantiate()
 	
-	var pos_y = randf_range(
-		get_viewport_rect().position.y,	
-		get_viewport_rect().end.y
+	var pos_y: float = randf_range(
+		upper_point.position.y,
+		lower_point.position.y
 	)
 	
-	new_pipe.position = Vector2(500, pos_y)
+	new_pipe.position = Vector2(
+		upper_point.position.x, 
+		pos_y)
 	
-	add_child(new_pipe)
+	pipes_holder.add_child(new_pipe)
 	
 func _on_timer_timeout() -> void:
 	spawn_pipes()
