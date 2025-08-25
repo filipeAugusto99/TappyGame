659f65f259fa0f84ee0d34b345760d2798910771
Filipe Augusto <filipecordeiro213@gmail.com>
2025-08-25 13:18:42 -0300

    Custom Resources

--- Globals/ScoreManager.gd
+++ Globals/ScoreManager.gd
@@ -1,5 +1,9 @@
 extends Node
 
+
+const SCORES_PATH: String = 'user://tappy.tres'
+
+
 var _high_score: int = 0
 
 var high_score: 
@@ -8,7 +12,21 @@ var high_score:
 	set(value):
 		if value > _high_score:
 			_high_score = value
+			save_high_score()
 
 
 func _ready() -> void:
-	pass
+	load_high_score()
+	
+
+func load_high_score() -> void:
+	if ResourceLoader.exists(SCORES_PATH) == true:
+		var hsr:  HighScoreResource = load(SCORES_PATH)
+		if hsr:
+			_high_score = hsr.high_score
+
+
+func save_high_score() -> void:
+	var hsr: HighScoreResource = HighScoreResource.new()
+	hsr.high_score = _high_score
+	ResourceSaver.save(hsr, SCORES_PATH)
